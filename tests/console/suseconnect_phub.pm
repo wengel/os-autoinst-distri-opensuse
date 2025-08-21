9e516da43224c15985921a928a584a77fa350502
Wolfgang Engel <wolfgang.engel@suse.com>
2025-08-21 17:46:59 +0200

    Adding suseconnect for activating Package Hub

---
+++ tests/console/suseconnect_phub.pm
@@ -0,0 +1,58 @@
+# SUSE's openQA tests
+#
+# Copyright 2018 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+
+# Package: SUSEConnect
+# Summary: Test SUSEConnect by registering system, module and deregistration.
+# - De-register system using SUSEConnect (-d, --cleanup,  --status-text)
+# - Register product using SCC_REGCODE code
+# - Add live patching if SLE < 15 using SCC_REGCODE_LIVE
+# - Check registering status
+# Maintainer: Juraj Hura <jhura@suse.com>
+
+use base "consoletest";
+use strict;
+use warnings;
+use testapi;
+use serial_terminal 'select_serial_terminal';
+use utils 'zypper_call';
+use registration;
+use version_utils 'is_sle';
+
+sub run {
+    my $reg_code = get_required_var("SCC_REGCODE");
+    my $arch = get_required_var("ARCH");
+    my $live_reg_code = get_required_var("SCC_REGCODE_LIVE");
+
+    select_serial_terminal;
+
+    # Make sure to start with de-registered system. In case the system is not registered this command will fail
+    assert_script_run "SUSEConnect -d || SUSEConnect --cleanup";
+    assert_script_run "SUSEConnect --status-text";
+
+    zypper_call('lr', exitcode => [0, 6]);
+    zypper_call 'services';
+    zypper_call 'products';
+
+    assert_script_run "SUSEConnect -r $reg_code", 180;
+    assert_script_run "SUSEConnect --status-text| grep -v 'Not Registered'";
+    zypper_call 'ref';
+    assert_script_run "SUSEConnect --list-extensions";
+
+    add_suseconnect_product(is_sle('<15') ? 'sle-live-patching' : 'sle-module-live-patching', undef, undef, "-r $live_reg_code");
+
+    assert_script_run "suseconnect -p PackageHub/16.0/x86_64";
+    assert_script_run "suseconnect -l";
+
+    assert_script_run "SUSEConnect --status";
+    assert_script_run "SUSEConnect -d || SUSEConnect --cleanup";
+    assert_script_run "SUSEConnect --status-text";
+
+}
+
+sub test_flags {
+    return {always_rollback => 1};
+}
+
+1;
