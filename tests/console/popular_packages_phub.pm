7963ad1e68190ff28dbbaeeb887745f16da2fb0d
Wolfgang Engel <Wolfgang.Engel@suse.com>
2025-11-24 16:58:26 +0100

    Fix typo

--- tests/console/popular_packages_phub.pm
+++ tests/console/popular_packages_phub.pm
@@ -18,7 +18,7 @@ use version_utils 'is_sle';
 
 sub run {
 	#my $popular_packages = get_required_var("PACKAGES");
-    my @popular_packages = ("vlc", "chromium", "libreoffice", "wine", "blueman", "libqca-qt5-2", "qca-qt5", "coolkey", "gnome-icon-theme", "0ad", "evolution-data-server", "python3-threadpoolctl", "prosody", "tumbler", "cockpit-storaged", "exim", "cyrus-imapd", "perl-Sys-Mmap", "php8-redis", "dleyna-server", "python313-drgn", "criu", "blender", "fail2ban", "opentofu", "openQA", "telegraf", "rspamd", "ubikey-manager", "sassist", "intel-graphics-compiler", "intel-opencl", "rshim", "obex-data-server", "leveldb", "perl-App-cpanminus", "audacity", "python3-pytest", "tesseract-ocr", "ansible-test", "uwsgi", "uwsgi-python3");
+    my @popular_packages = ("vlc", "chromium", "libreoffice", "wine", "blueman", "libqca-qt5-2", "qca-qt5", "coolkey", "gnome-icon-theme", "0ad", "evolution-data-server", "python3-threadpoolctl", "prosody", "tumbler", "cockpit-storaged", "exim", "cyrus-imapd", "perl-Sys-Mmap", "php8-redis", "dleyna-server", "python313-drgn", "criu", "blender", "fail2ban", "opentofu", "openQA", "telegraf", "rspamd", "yubikey-manager", "sassist", "intel-graphics-compiler", "intel-opencl", "rshim", "obex-data-server", "leveldb", "perl-App-cpanminus", "audacity", "python3-pytest", "tesseract-ocr", "ansible-test", "uwsgi", "uwsgi-python3");
     my $arch = get_required_var("ARCH");
 
     select_serial_terminal;
