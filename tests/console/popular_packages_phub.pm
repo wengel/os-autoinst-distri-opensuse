631ceb231d1edeefe700f44c109557a104409e98
Wolfgang Engel <Wolfgang.Engel@suse.com>
2026-03-23 11:08:25 +0100

    Add logic to show failed packages

--- tests/console/popular_packages_phub.pm
+++ tests/console/popular_packages_phub.pm
@@ -20,6 +20,7 @@ sub run {
 	#my $popular_packages = get_required_var("PACKAGES");
     my @popular_packages = ("vlc", "chromium", "libreoffice", "wine", "blueman", "libqca-qt5-2", "qca-qt5", "coolkey", "gnome-icon-theme", "0ad", "evolution-data-server", "python3-threadpoolctl", "prosody", "tumbler", "cockpit-storaged", "exim", "cyrus-imapd", "perl-Sys-Mmap", "php8-redis", "dleyna-server", "python313-drgn", "criu", "blender", "fail2ban", "opentofu", "openQA", "telegraf", "rspamd", "yubikey-manager", "sassist", "intel-opencl", "rshim", "obex-data-server", "leveldb", "perl-App-cpanminus", "audacity", "python3-pytest", "tesseract-ocr", "ansible-test", "uwsgi", "uwsgi-python3");
     my $arch = get_required_var("ARCH");
+    my @popular_packages_fail = ();
 
     select_serial_terminal;
 
@@ -30,10 +31,18 @@ sub run {
     my $package = ();
     foreach $package (@popular_packages)
     {
-      zypper_call "in $package";
+      if ((zypper_call("in $package", exitcode => [0, 104] == 104)) {
+          push(@popular_packages_fail, $package);
+          return 0;
+      }
     }
-
-}
+    my $package_fail = ();
+    if (@popular_packages_fail) {
+      foreach my $package_fail (@popular_packages_fail) {
+        print "$package_fail";
+      }
+      return 1;
+    }
 
 sub test_flags {
     return {always_rollback => 1};
