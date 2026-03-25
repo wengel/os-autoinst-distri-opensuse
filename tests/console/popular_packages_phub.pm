930e76ca6049df85c07bd27b42c7b30d67a97ebd
Wolfgang Engel <Wolfgang.Engel@suse.com>
2026-03-25 16:59:36 +0100

    Get packages from command line

--- tests/console/popular_packages_phub.pm
+++ tests/console/popular_packages_phub.pm
@@ -18,8 +18,9 @@ use version_utils 'is_sle';
 
 sub run {
 	#my $popular_packages = get_required_var("PACKAGES");
-    my @popular_packages = ("vlc", "chromium", "libreoffice", "wine", "blueman", "libqca-qt5-2", "qca-qt5", "coolkey", "gnome-icon-theme", "0ad", "evolution-data-server", "python3-threadpoolctl", "prosody", "tumbler", "cockpit-storaged", "exim", "cyrus-imapd", "perl-Sys-Mmap", "php8-redis", "dleyna-server", "python313-drgn", "criu", "blender", "fail2ban", "opentofu", "openQA", "telegraf", "rspamd", "yubikey-manager", "sassist", "intel-opencl", "rshim", "obex-data-server", "leveldb", "perl-App-cpanminus", "audacity", "python3-pytest", "tesseract-ocr", "ansible-test", "uwsgi", "uwsgi-python3");
+    # my @popular_packages = ("vlc", "chromium", "libreoffice", "wine", "blueman", "libqca-qt5-2", "qca-qt5", "coolkey", "gnome-icon-theme", "0ad", "evolution-data-server", "python3-threadpoolctl", "prosody", "tumbler", "cockpit-storaged", "exim", "cyrus-imapd", "perl-Sys-Mmap", "php8-redis", "dleyna-server", "python313-drgn", "criu", "blender", "fail2ban", "opentofu", "openQA", "telegraf", "rspamd", "yubikey-manager", "sassist", "intel-opencl", "rshim", "obex-data-server", "leveldb", "perl-App-cpanminus", "audacity", "python3-pytest", "tesseract-ocr", "ansible-test", "uwsgi", "uwsgi-python3");
     my $arch = get_required_var("ARCH");
+    my @popular_packages = split(',' get_required_var("POPULAR_PACKAGES");
     my @popular_packages_fail = ();
 
     select_serial_terminal;
